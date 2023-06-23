d2782a6216869cd7b15af2c267f6ca30c6dfde9f
levanhai1998 <86667736+levanhai1998@users.noreply.github.com>
2023-06-23 21:59:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '💘bé huyền💘';
+const giftUrl = '';
+const eventName = 'Chúc mừng sinh nhật';
+const titleCard = 'Tặng bé huyền';
+const contentCard = 'Chúc em sinh nhật vui vẻ, luôn tươi trẻ và xinh đẹp, gặp nhiều may mắn trong công việc 💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
